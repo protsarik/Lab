380e981003651914a1a0e1ac944cd2f53e35618f
nikita <lordnikitas@gmail.com>
2020-03-15 22:19:48 +0300

    10 to int

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,7 @@ public class Main {
 class TwoNumberSum {
 
     public static void main(String[] args) {
-        int num1=12;
+        int num1=10;
         int num2=15;
         int sum= num1 + num2;
         System.out.print("Sum is:");

ac7dcf80c4ec24f822608bd98acc28980b63db33
protsarik <lordnikitas@gmail.com>
2020-03-15 22:07:25 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+    }
+}
+class TwoNumberSum {
+
+    public static void main(String[] args) {
+        int num1=12;
+        int num2=15;
+        int sum= num1 + num2;
+        System.out.print("Sum is:");
+        System.out.println(sum);
+    }
+}
+class HelloMain {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello%2d and %6s",8,"Hi!\n");
+        System.out.printf("Hello%2d and %6s",8,"Hi!\n");
+    }
+}

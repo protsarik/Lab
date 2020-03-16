2412424383515e53af07c3700b7c3c8d81649905
nikita <lordnikitas@gmail.com>
2020-03-16 23:02:56 +0300

    Mytest first

---
+++ src/com/zzz/geometry/Circle.java
@@ -0,0 +1,7 @@
+package com.zzz.geometry;
+
+public class  Circle{
+    public String toString(){
+        return "This is a Circle";
+    }
+}
---
+++ src/mytest/Test.java
@@ -0,0 +1,16 @@
+package mytest;
+
+import com.zzz.geometry.Circle;
+import com.zzz.geometry.Cyclinder;
+import com.yyy.animal.Cat;
+
+public class Test {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle);
+        Cyclinder cyclinder = new Cyclinder();
+        System.out.println(cyclinder);
+        Cat cat = new Cat();
+        System.out.println(cat);
+    }
+}

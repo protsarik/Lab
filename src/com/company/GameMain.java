7a0f2d750e64c2ba276fb4e566a1e997453213b2
nikita <lordnikitas@gmail.com>
2020-03-16 21:21:28 +0300

    random

---
+++ src/com/company/GameMain.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class GameMain   {
+  public static int COLS=3;
+  public static int ROWS=3;
+}
+
---
+++ src/com/company/GamePanel.java
@@ -0,0 +1,13 @@
+package com.company;
+import static com.company.GameMain.ROWS;
+import static com.company.GameMain.COLS;
+    public class GamePanel{
+        public static void main(String[] args){
+    /*int [][] score=int[ROWS][COLS];*/
+    for(int row=0;row<ROWS;++row){
+        for (int col=0;col<COLS;++col){
+             }
+        }
+    System.out.println(ROWS);
+        }
+    }

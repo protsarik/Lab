2345e5cd521d0c78798b8ad983dc4fe0886b23c0
protsarik <lordnikitas@gmail.com>
2020-03-16 17:35:15 +0300

    Новый класс NoImport

---
+++ src/com/company/ScannerNoImport.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class ScannerNoImport {
+
+    public static void main(String[] args) {
+        java.util.Scanner in = new java.util.Scanner(System.in);
+        System.out.print("Enter a integer:");
+        int number= in.nextInt();
+        System.out.print("You have a integer "+ number);
+    }
+}

98b4d197fbb09f2211665b9d2cffa78bf73b66cd
Maksim <maks_igntaenko_2001@mail.ru>
2020-03-17 16:50:47 +0300

    Поменял переменную COLS

---
+++ src/com/individual/Indi6.java
@@ -0,0 +1,11 @@
+package com.individual;
+
+public class Indi6 {
+    public static void main(String[] args) {
+        int x = 5;
+        double vershki = x * 22.497;
+        double fut = x * 3.28084;
+        System.out.println("znach v ver: " + vershki + " ver");
+        System.out.println("znach v fut: " + fut + " fut");
+    }
+}
--- src/com/individual/Indi9.java
+++ src/com/individual/Indi9.java
@@ -1,8 +1,16 @@
 package com.individual;
 
+import java.util.Scanner;
+
 public class Indi9 {
     public static void main(String[] args) {
-        int x = 10;
-        double san = x;
-    }
+        Scanner w = new Scanner(System.in);
+        System.out.println("vvedi x: ");
+        double x = w.nextDouble();
+
+        double san = x * 100;
+        double dume = x * 39.3700787;
+        System.out.println("znach v san: " + san + " sm");
+        System.out.println("znach v dumah: " + dume + " dume");
+}
 }

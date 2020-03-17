552851625f643fbc189aae3888f36aa83acc2a16
protsarik <lordnikitas@gmail.com>
2020-03-17 16:03:20 +0300

    Выполнил инди

--- src/com/individual/SrVes.java
+++ src/com/individual/SrVes.java
@@ -7,14 +7,31 @@ public class SrVes {
         Scanner ves1 = new Scanner(System.in);
         System.out.print("VVedi ves X: ");
         double vesX = ves1.nextDouble();
+
         Scanner ves2 = new Scanner(System.in);
         System.out.print("VVedi ves Y: ");
         double vesY = ves2.nextDouble();
+
         Scanner ves3 = new Scanner(System.in);
         System.out.print("VVedi ves Z: ");
         double vesZ = ves3.nextDouble();
-        System.out.println(vesX + "pudov");
-        System.out.println(vesY + "futov");
-        System.out.println(vesZ + "kg");
+
+        var vesP2 = vesY / 40;
+        var vesP3 = vesZ/16.38;
+        double srZnPudr= (vesX+vesP2+vesP3)/3;
+
+        var vesF1 = vesX*40;
+        var vesF3 = vesZ*2.44;
+        double srZnFut= (vesF1+vesY+vesF3)/3;
+
+        var vesK1 = vesX*16.38;
+        var vesK2 = vesY/2.44;
+        double srZnKg= (vesK1+vesK2+vesZ)/3;
+
+
+        System.out.println(srZnPudr + " pudov");
+        System.out.println(srZnFut + " futov");
+        System.out.println(srZnKg + " kg");
+
     }
 }

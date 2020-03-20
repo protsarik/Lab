b9bc4cd53bc333c9475e52c50d7fd99fb10ffeb9
protsarik <lordnikitas@gmail.com>
2020-03-20 19:07:25 +0300

    num3 начало

--- src/Lab2/zd1/Num3.java
+++ src/Lab2/zd1/Num3.java
@@ -1,4 +1,15 @@
 package Lab2.zd1;
 
+import java.util.Scanner;
+
 public class Num3 {
+    public static void main(String[] args){
+        int days=0;
+        Scanner ves = new Scanner(System.in);
+        System.out.print("Введи месяц: ");
+        double n = ves.nextDouble();
+        switch ("В этом месяце "+days+" дней"){
+            case 0:
+        }
+    }
 }

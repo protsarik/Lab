e8516297130681d42b8e86b18edf40d3e2f11df3
Maksim <maks_igntaenko_2001@mail.ru>
2020-03-23 19:39:32 +0300

    изменил

--- src/Lab_2_Ignatos/Z_2_3.java
+++ src/Lab_2_Ignatos/Z_2_3.java
@@ -1,5 +1,5 @@
 package Lab_2_Ignatos;
-import java.util.Scanner;
+/*import java.util.Scanner;
 
 public class Z_2_3 {
     public static void main(String[] args){
@@ -14,9 +14,29 @@ public class Z_2_3 {
         {
             if ( i % 7 == 0)
             {
-                System.out.println(i);
+                System.out.println(i + " ");
             }
            else System.out.println("Chisel kratnyh 7 net!!!");
         }
+    }
+}*/
+ import java.util.Scanner;
+public class Z_2_3 {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int result = 0;
+        int M=0;
+        for (int i = A; i < B; i++){
+
+            if(i % 7 == 0)
+            {
+                M = i;
+                result++;
+            }
+        }
+        System.out.println("M = " + M);
+        System.out.println(result);
     }
 }

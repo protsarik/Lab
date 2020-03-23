19d233c5aae69bcf884911a47ea410ceea6dc323
protsarik <lordnikitas@gmail.com>
2020-03-23 19:47:45 +0300

    Правки третьего

--- src/Lab_2_Ignatos/Z_2_3.java
+++ src/Lab_2_Ignatos/Z_2_3.java
@@ -1,42 +1,19 @@
 package Lab_2_Ignatos;
-/*import java.util.Scanner;
+
+import java.util.Scanner;
 
 public class Z_2_3 {
-    public static void main(String[] args){
-        Scanner per = new Scanner(System.in);
-        System.out.print("Vvedite otrezok ot A do B : ");
-        System.out.print("Vvedite A: ");
-        int A = per.nextInt();
-        System.out.print("Vvedite B: ");
-        int B = per.nextInt();
-        int i;
-        for(i = A; i <= B; i++);
-        {
-            if ( i % 7 == 0)
-            {
-                System.out.println(i + " ");
-            }
-           else System.out.println("Chisel kratnyh 7 net!!!");
-        }
-    }
-}*/
- import java.util.Scanner;
-public class Z_2_3 {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи А");
         int A = scanner.nextInt();
+        System.out.println("Введи В");
         int B = scanner.nextInt();
-        int result = 0;
-        int M=0;
-        for (int i = A; i < B; i++){
-
-            if(i % 7 == 0)
-            {
-                M = i;
-                result++;
+        for (int i = A; i <= B; i++) {
+            System.out.println("Результат ");
+            if (i % 7 == 0) {
+                System.out.print(" " + i);
             }
         }
-        System.out.println("M = " + M);
-        System.out.println(result);
     }
 }

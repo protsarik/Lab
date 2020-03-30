f24973ddb6d54da5fa21fc01165886dbe0d9bf80
nikita <lordnikitas@gmail.com>
2020-03-30 22:05:45 +0300

    Правки небольшие

--- src/Lab3/Primer_8.java
+++ src/Lab3/Primer_8.java
@@ -2,7 +2,7 @@ package Lab3;
 
 
 public class Primer_8 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello, " + args[0] + "! It's a work! ");
     }
 }

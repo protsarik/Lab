69e6bfc1eebd8e501c814c31eec1604af7b48ca5
Nikita <lordnikitas@gmail.com>
2020-04-02 19:24:41 +0300

    Выполнено инди

--- src/Lab3/IndiTsar.java
+++ src/Lab3/IndiTsar.java
@@ -6,21 +6,23 @@ import java.util.Scanner;
 
 public class IndiTsar {
     public static void main(String[] args) {
-        double f1 = 0;
-        double n = 0;
+        double n;
         double f2 = 0;
+        double f1 = 0;
         try {
             System.out.println("Выводим вещественные числа иp файла");
             Scanner in = new Scanner(new File("src/Lab3/float.txt"));
+            f1 = in.nextDouble();
             System.out.println("Файл найден");
             while (in.hasNext()) {
-                f2 =in.nextDouble();
+                f2 = in.nextDouble();
             }
+            in.close();
         } catch (FileNotFoundException ex) {
             System.out.println("А файл тебе значит не нужен?...");
         } finally {
             n = f1 - f2;
-            System.out.println("ssss= "+ n);
+            System.out.println("Разность= " + n);
         }
     }
 }

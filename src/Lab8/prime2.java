34ba5db144f3beee1a387e59b2dba7df7b372ad1
Nikita <lordnikitas@gmail.com>
2020-04-18 10:26:23 +0300

    масиивы

---
+++ src/Lab8/prime2.java
@@ -0,0 +1,51 @@
+package Lab8;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class prime2 extends JFrame {
+    private JButton button = new JButton("Нажми");
+    private JTextField input = new JTextField("", 5);
+    private JLabel label = new JLabel("Введи:");
+    private JRadioButton radio1 = new JRadioButton("Выбери 1");
+    private JRadioButton radio2 = new JRadioButton("Выбери 2");
+    private JCheckBox check = new JCheckBox("Флаг", false);
+
+    public prime2() {
+        super("Простой пример");
+        this.setBounds(100, 100, 250, 100);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container container = this.getContentPane();
+        container.setLayout(new GridLayout(3, 2, 2, 2));
+        container.add(label);
+        container.add(input);
+        ButtonGroup group = new ButtonGroup();
+        group.add(radio1);
+        group.add(radio2);
+        container.add(radio1);
+        radio1.setSelected(true);
+        container.add(radio2);
+        container.add(check);
+        button.addActionListener(new ButtonEventListener());
+        container.add(button);
+    }
+
+    class ButtonEventListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String message = "";
+            message += "Кнопка была нажата\n";
+            message += "Текст: " + input.getText() + "\n";
+            message += (radio1.isSelected() ? "Radio #1" : "Radio #2") + " выбрано\n";
+            message += "CheckBox был " + ((check.isSelected()) ? "установлен" :
+                    "не установлен");
+            JOptionPane.showMessageDialog(null, message, "Output",
+                    JOptionPane.PLAIN_MESSAGE);
+        }
+    }
+
+    public static void main(String[] args) {
+        prime2 app = new prime2();
+        app.setVisible(true);
+    }
+}
--- src/Lab_5/p2.java
+++ src/Lab_5/p2.java
@@ -1,26 +1,8 @@
 package Lab_5;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class p2 {
     public static void main(String[] args) {
-        int size = 20;
-        int[][] array = {{180,80},{150,100},{190,50}};
-
-//        try {
-//            System.out.println("Выводим массив из файла");
-//            Scanner in = new Scanner(new File("src/Lab_5/intMassive.txt"));
-//            while (in.hasNext()) {
-//                array[0][1] = in.nextInt();
-//            }
-//            System.out.println();
-//            in.close();
-//        } catch (
-//                FileNotFoundException ex) {
-//            System.out.println("А файл тебе значит не нужен?...");
-//        }
+        int[][] array = {{180, 80}, {150, 100}, {190, 50}};
         int p = 0;
         int count = 0;
         System.out.println("Следующие кандидаты не подходят:");

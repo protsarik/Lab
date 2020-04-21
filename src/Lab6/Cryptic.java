eb050dbaf252ec7ea9250bac207debf2e15f640c
nikita <lordnikitas@gmail.com>
2020-04-21 20:54:46 +0300

    Закончил 6 лабу

--- src/Lab6/Cryptic.java
+++ src/Lab6/Cryptic.java
@@ -30,6 +30,14 @@ public class Cryptic {
         }
         text = new String(ArrayWord);
         text = "A1 " + text;
+        try {
+            Formatter out = new Formatter(new File("src/Lab6/crypt.txt"));
+            out.format(text);
+            out.close();
+            System.out.println("Запись выполнена.");
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
         return this.text;
     }
 
@@ -44,23 +52,6 @@ public class Cryptic {
         }
         text = new String(ArrayWord);
         text = "A2 " + text;
-        return this.text;
-    }
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите текст:");
-        String text = in.nextLine();
-        System.out.print("Введите номер метода шифрования:");
-        int MethodCrypt = in.nextInt();
-
-        switch(MethodCrypt){
-            case 1:Cryptic crypt = new Cryptic(text);
-                text = crypt.CryptA1();break;
-            case 2:Cryptic crypt2 = new Cryptic(text);
-                text = crypt2.CryptA2();break;
-        }
-        System.out.println(text);
         try {
             Formatter out = new Formatter(new File("src/Lab6/crypt.txt"));
             out.format(text);
@@ -69,6 +60,71 @@ public class Cryptic {
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
         }
+        return this.text;
+    }
 
+    public String Encrypt() {
+        try {
+            Scanner in = new Scanner(new File("src/Lab6/crypt.txt"));
+            text = in.nextLine();
+
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        if (text.startsWith("A1")) {
+            text = text.substring(3);
+            char[] ArrayWord = text.toCharArray();
+            int n = ArrayWord.length;
+            char temp;
+            for (int i = 0; i < n - 1; i = i + 2) {
+                temp = ArrayWord[i + 1];
+                ArrayWord[i + 1] = ArrayWord[i];
+                ArrayWord[i] = temp;
+            }
+            text = new String(ArrayWord);
+        } else if (text.startsWith("A2")) {
+            text = text.substring(3);
+            char[] ArrayWord = text.toCharArray();
+            int n = ArrayWord.length;
+            char temp;
+            for (int i = 0; i < n / 2; i++) {
+                temp = ArrayWord[n - i - 1];
+                ArrayWord[n - i - 1] = ArrayWord[i];
+                ArrayWord[i] = temp;
+            }
+            text = new String(ArrayWord);
+        } else System.out.println("Текст не зашифрован доступными методами");
+        return text;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите номер метода (де-)шифрования");
+        System.out.println("A1-1-й алгоритм; А2-2-й алгоритм; 3-дешифровка");
+        int MethodCrypt = in.nextInt();
+        String text = null;
+
+        switch (MethodCrypt) {
+            case 1:
+                Scanner t = new Scanner(System.in);
+                System.out.print("Введите текст:");
+                text = t.nextLine();
+                Cryptic crypt1 = new Cryptic(text);
+                text = crypt1.CryptA1();
+                break;
+            case 2:
+                Scanner v = new Scanner(System.in);
+                System.out.print("Введите текст:");
+                text = v.nextLine();
+                Cryptic crypt2 = new Cryptic(text);
+                text = crypt2.CryptA2();
+                break;
+            case 3:
+                Cryptic encrypt = new Cryptic(text);
+                text = encrypt.Encrypt();
+                break;
+        }
+        System.out.println("Итоговый текст: " + text);
     }
 }

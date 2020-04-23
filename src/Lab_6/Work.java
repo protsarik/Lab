2ca0683f4aa8b7911d71c3f0331519e4cf39f576
Maksim <maks_igntaenko_2001@mail.ru>
2020-04-23 23:30:08 +0300

    OOOOOOPPssss Z2 3/4

--- src/Lab_6/Work.java
+++ src/Lab_6/Work.java
@@ -4,12 +4,18 @@ package Lab_6;
 // Для сотрудников, работающих по договору подряда заработная плата начисляется исходя из ставки за час и количества отработанных.
 //Написать программу для начисления заработной платы и формирования ведомости для выдачи заработной платы.
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Formatter;
+import java.util.Scanner;
+
 public class Work {
     private double oklad;
     private double nadbabka;
     private double premiya;
     private int time = 176;
     private double Z;
+    private double stavka;
 
 
     public Work(double okl, double nadb, double prem, int times) {
@@ -28,9 +34,61 @@ public class Work {
         return Z;
     }
 
-    public static void main(String[] args) {
+    public Work(double stavka, int time) {
+        this.stavka = stavka;
+        this.time = time;
+    }
 
-        Work w1 = new Work(10000, 0.15, 5000, 170);
-        System.out.println("Z = " + w1.Zarplata());
+    public double ZP() {
+        Z = stavka * time;
+        return Z;
+    }
+
+    public double getZP(){
+        return Z;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Выберите: 1 - штатник 2 - подрядчик 3 - ведомость за ЗП");
+        Scanner s = new Scanner(System.in);
+        int ss = s.nextInt();
+        switch (ss) {
+            case 1:
+                Work w1 = new Work(10000, 0.15, 5000, 170);
+                System.out.println("Zш = " + w1.Zarplata());
+                try {
+                    Formatter out = new Formatter(new File("src/Lab_6/work.txt"));
+                    out.format(String.valueOf("ш - " + w1.getZP() + "\n"));
+                    out.close();
+                    System.out.println("Запись выполнена.");
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+                break;
+            case 2:
+                Work w2 = new Work(10, 176);
+                System.out.println("Zп = " + w2.ZP());
+                try {
+                    Formatter out = new Formatter(new File("src/Lab_6/work.txt"));
+                    out.format(String.valueOf("п - " + w2.getZP() + "\n"));
+                    out.close();
+                    System.out.println("Запись выполнена.");
+                }  catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
+                break;
+            case 3:
+                try {
+                    Scanner read = new Scanner(new File("src/Lab_6/work.txt"));
+                    String text = read.nextLine();
+                    System.out.println(text);
+                    read.close();
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+
+        }
+
+
     }
 }
